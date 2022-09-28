055f7c8f5b830f453af4551edee4354d22754529
The Coder <82015917+thecoderlive@users.noreply.github.com>
2022-09-29 01:53:49 +0530

    Initial Commit for file: DATA.js

---
+++ DATA.js
@@ -0,0 +1,145 @@
+export const item = {
+    "id": "dc374b1d-ce1f-43b7-9432-0f72752d7086",
+    "home_feed": [
+        {
+            "id": "e4180993-4642-46de-83b6-4c5effe8caa3",
+            "artist_name": "note",
+            "home_carousel": [
+                {
+                    "id": "32611d0d-5761-4d18-8c73-ca48251edd69",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/3.png"
+                },
+                {
+                    "id": "5bfeb547-3e8f-487a-8819-d99d916b3f15",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/20.png"
+                },
+                {
+                    "id": "aa8d796f-3175-4147-8072-f04777e8d2b4",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/15.png"
+                },
+                {
+                    "id": "88771cef-7eee-4d01-a1d6-4c847ff58cb7",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/135.png"
+                },
+                {
+                    "id": "91e4eb50-a0a4-4495-9c98-bcd7661091d7",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/251.png"
+                }
+            ],
+            "art_caption": "continue",
+            "art_description": "Admit may get remember learn activity wife. Sense wife most game student third manage after."
+        },
+        {
+            "id": "fafb6987-a46f-48eb-8ed2-96b0a6757521",
+            "artist_name": "school",
+            "home_carousel": [
+                {
+                    "id": "02846f43-a528-4af5-86f5-2276a74a70a6",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/359.png"
+                },
+                {
+                    "id": "dae34c7f-2032-4dbf-ba6e-acb160e1dd86",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/263.png"
+                },
+                {
+                    "id": "abf19ce0-e1ac-487c-b7fc-fda810210f25",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/156.png"
+                },
+                {
+                    "id": "1d61a8cf-bdc3-4b2c-a4b8-66c18d84acc8",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/93.png"
+                },
+                {
+                    "id": "28703ee1-056c-4bc3-8a99-76446a3a67d5",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/291.png"
+                }
+            ],
+            "art_caption": "arrive",
+            "art_description": "Work itself increase wide interesting. Item relationship spend box among majority. Behind trade life approach TV."
+        },
+        {
+            "id": "02cf1a79-b726-4fe5-b1d4-59e4798c7e9e",
+            "artist_name": "herself",
+            "home_carousel": [
+                {
+                    "id": "9df794c0-237f-4b5e-bf6b-94733d6bb8c2",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/282.png"
+                },
+                {
+                    "id": "f22d21de-1e72-41e1-be19-2e0029660524",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/121.png"
+                },
+                {
+                    "id": "ff55bd2c-99b2-4538-9709-4b86de7d423a",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/229.png"
+                },
+                {
+                    "id": "68a89a92-c580-4863-811e-574b57a70c41",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/114.png"
+                },
+                {
+                    "id": "2c059713-73f9-4213-9f9d-32325edb153a",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/80.png"
+                }
+            ],
+            "art_caption": "order",
+            "art_description": "Most agency face around rather end soon. Before president conference suffer. Thought address explain case hair."
+        },
+        {
+            "id": "036c9c10-1081-46c5-bb7c-acec479bfcf9",
+            "artist_name": "seven",
+            "home_carousel": [
+                {
+                    "id": "a81665db-97a4-48dc-aa50-2426d4b5fd91",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/361.png"
+                },
+                {
+                    "id": "f81b0870-535b-4806-a1b7-8d3bf2916868",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/306.png"
+                },
+                {
+                    "id": "5a7d23a6-7fcf-41c3-b15e-7b8c73e3c056",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/64.png"
+                },
+                {
+                    "id": "2a92d1a2-44cd-48a6-a393-0f818ca22c7b",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/290.png"
+                },
+                {
+                    "id": "f85b44ac-7eec-4abd-9c65-681acd67ab05",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/62.png"
+                }
+            ],
+            "art_caption": "believe",
+            "art_description": "Moment purpose war. Focus federal imagine. Contain case artist allow fight age see. Tonight listen officer once."
+        },
+        {
+            "id": "87dc6f54-b8fc-4984-bae8-69c1d6d416ca",
+            "artist_name": "either",
+            "home_carousel": [
+                {
+                    "id": "9c9be38d-d1e0-4b59-b622-3b54acd051b5",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/250.png"
+                },
+                {
+                    "id": "01003a36-8ad1-46c6-be22-b8eb762de8f4",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/15.png"
+                },
+                {
+                    "id": "3a0af567-2a81-4109-8e34-b137f670b6e6",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/2.png"
+                },
+                {
+                    "id": "a04ba621-7ab9-409f-9652-57ba9c6d72fd",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/203.png"
+                },
+                {
+                    "id": "21f52241-35da-4e3a-9863-34ef4d17ce95",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/159.png"
+                }
+            ],
+            "art_caption": "listen",
+            "art_description": "Available know real place crime process. Successful entire pass. Yeah receive place several produce compare meet risk. Operation system occur door several war mouth."
+        }
+    ]
+}
